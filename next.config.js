152199863afbeeb610f1294c69c729f76709c357
theborakompanioni <theborakompanioni+github@gmail.com>
2024-08-28 15:09:01 +0200

    build(gh-pages): remove unnecessary nextjs props

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // change base path to make deployment on gh-pages work properly
-    basePath: process.env.NODE_ENV === 'production' ? '' : undefined,
-    reactStrictMode: true,
-
     output: 'export',
 
     // must be done when using `output := 'export'`
